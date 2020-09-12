e4678297427b8bbba6a899fba4a60d7f0d96ca90
MichelLopez <michel@lopez.nom.fr>
2020-09-12 14:07:41 -0700

    Update test_lib2.py

--- lib/test_lib2.py
+++ lib/test_lib2.py
@@ -1,4 +1,24 @@
-#version : 2020-09-10-001
+#version : 2020-09-12-001
+
+def test_funct():
+    msg = 'this is from the test function2'
+    return msg
+
+def test_funct():
+    msg = 'this is from the test function2'
+    return msg
+
+def test_funct():
+    msg = 'this is from the test function2'
+    return msg
+
+def test_funct():
+    msg = 'this is from the test function2'
+    return msg
+
+def test_funct():
+    msg = 'this is from the test function2'
+    return msg
 
 def test_funct():
     msg = 'this is from the test function2'
